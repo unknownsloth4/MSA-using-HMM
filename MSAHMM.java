83b02180f2f8fd69a62bdc4363c492799147c8f9
unknownsloth4 <43465658+unknownsloth4@users.noreply.github.com>
2018-10-14 08:23:44 +0700

    Add files via upload

---
+++ MSAHMM.java
@@ -0,0 +1,59 @@
+public class MSAHMM {
+	
+	public static void main(String args[]){  
+	
+	String car[][] = new String[][] {
+					{"X", "X", "Y"},
+					{"X", "X", "X"},
+					{"X", "X", "X"}
+					};
+					
+		for (i=0; i<car.length; i++) {
+			if (car[i+1].length == car[i].length) {
+				continue;
+			} else {
+				if (car[i+1].length < car[i].length){
+					selisih = car[i].length - car[i+1].length;
+					flag = 0;
+					
+					for (i=0; i<car.length; i++) {
+						for (j=0; j<car[0].length; j++) {
+							if (car.charAt[i][j] == car.charAt[i+1][j]) {
+								continue;
+							} else {
+								String gap = "-";
+								str.insert ([i+1][j], gap);
+								flag = flag + 1;
+							}
+							
+							if (flag == selisih) {
+								break;
+							}
+						}
+					}
+				} else {
+					if (car[i+1].length > car[i].length) {
+						for (i=0; i<car.length; i++) {
+							for (j=0; j<car[0].length; j++) {
+								if (car.charAt[i][j] == car.charAt[i+1][j])	{
+									continue;
+								} else {
+									for (i=car[0].length; i<car.length; i--) {
+										String gap = "-";
+										str.insert ([i-1][j], gap);
+										flag = flag + 1;
+									}
+								}
+								
+								if (flag == selisih) {
+									break;
+								}
+							}	
+						}
+					}
+				}
+			}
+		}
+	}
+					
+}
